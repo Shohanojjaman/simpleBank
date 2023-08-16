e4cfac8746c5bab04139099e2d12e1fded973150
Shohanojjaman Emon <shohanojjamanemon@gmail.com>
2023-08-16 09:28:42 +0600

    Added login validation.

---
+++ main.js
@@ -0,0 +1,10 @@
+document.getElementById('submit').addEventListener('click', function () {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  if (email === 'shohan@gmail.com' && password === 'Shohan404') {
+    location = 'bank.html';
+  } else {
+    alert('Invalid input.');
+  }
+});

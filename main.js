7a2a9585ef9d1504452e37a9fae9cad5a43e9a61
Shohanojjaman Emon <shohanojjamanemon@gmail.com>
2023-08-16 14:13:02 +0600

    Added deposit and withdraw calculation.

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
-document.getElementById('submit').addEventListener('click', function () {
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-
-  if (email === 'shohan@gmail.com' && password === 'Shohan404') {
-    location = 'bank.html';
-  } else {
-    alert('Invalid input.');
+// Deposit calculation
+document.getElementById('deposit-btn').addEventListener('click', function () {
+  let totalDepositAmountMain = document.getElementById('totalDepositAmount').innerText;
+  let totalDepositAmount = Number.parseFloat(totalDepositAmountMain);
+  let depositAmount = document.getElementById('depositAmount').value;
+  depositAmount = Number(depositAmount);
+  let totalBalanceAmountMain = document.getElementById('totalBalanceAmount').innerText;
+  let totalBalanceAmount = Number.parseFloat(totalBalanceAmountMain);
+  if (depositAmount === 0 || typeof depositAmount !== 'number') {
+    alert('Invalid input');
+    return;
   }
+  document.getElementById('totalDepositAmount').innerText = totalDepositAmount + depositAmount;
+
+  document.getElementById('totalBalanceAmount').innerText = totalBalanceAmount + depositAmount;
+  console.log(typeof totalBalanceAmount);
+
+  document.getElementById('depositAmount').value = '';
+});
+
+// Withdraw calculation
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+  let totalWithdrawAmountMain = document.getElementById('totalWithdrawAmount').innerText;
+  let totalWithdrawAmount = Number.parseFloat(totalWithdrawAmountMain);
+  let withdrawAmount = document.getElementById('withdrawAmount').value;
+  withdrawAmount = Number(withdrawAmount);
+  let totalBalanceAmountMain = document.getElementById('totalBalanceAmount').innerText;
+  let totalBalanceAmount = Number.parseFloat(totalBalanceAmountMain);
+  if (withdrawAmount === 0 || typeof withdrawAmount !== 'number') {
+    alert('Invalid input');
+    return;
+  }
+  document.getElementById('totalWithdrawAmount').innerText = totalWithdrawAmount + withdrawAmount;
+
+  document.getElementById('totalBalanceAmount').innerText = totalBalanceAmount - withdrawAmount;
+  console.log(typeof totalBalanceAmount);
+
+  document.getElementById('withdrawAmount').value = '';
 });
